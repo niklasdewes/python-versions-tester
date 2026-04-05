31b25f06799a5c504c2ff29d799fd90b35e96d9d
Niklas Dewes <niklas.dewes@gmail.com>
2026-04-05 21:56:03 +0200

    initial commit

--- build_images.sh
+++ build_images.sh
@@ -5,5 +5,5 @@ mkdir tests/$imageDirectory
 
 for pyver in $(cat versions.txt); do 
     outputFile="output_$pyver.txt"
-    docker build --build-arg python_version="$pyver" --tag mytest:$pyver . | tee tests/$imageDirectory/$outputFile
+    docker buildx build --build-arg python_version="$pyver" --tag mytest:$pyver . | tee tests/$imageDirectory/$outputFile
 done
